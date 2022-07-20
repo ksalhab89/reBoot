5241de5835ad4108c6095d7daf16f11d857919c2
Khaled Salhab <khaled.salhab@forescout.com>
2022-07-20 13:19:09 +0300

    -

---
+++ src/Ch01/E09.java
@@ -0,0 +1,9 @@
+package Ch01;
+
+public class E09 {
+	public static void main(String[] args) {
+		System.out.println("width = 4.5 and height = 7.9");
+		System.out.println("Area of rectangle = " + 4.5 * 7.9);
+		System.out.println("Perimeter of rectangle = " + 2 * 4.5 + 2 * 7.9);
+	}
+}
---
+++ src/Ch01/E10.java
@@ -0,0 +1,7 @@
+package Ch01;
+
+public class E10 {
+	public static void main(String[] args) {
+
+	}
+}
